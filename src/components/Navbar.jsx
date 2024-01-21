01e6dffe05a2a6a7ea19ea3f0387f7be9615c3a5
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-22 05:16:04 +0530

    added dependecies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="flex justify-between items-center"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#01051e",
+        "primary-color": "#321768",
+        "second-text-color": "#ada2c3",
+        "sub-heading": "#ffc50c",
+        "color-blob1": "#ff7d3b",
+        "color-blob": "#A427DF",
+      },
+
+      fontFamily: {
+        "primary-font": "Barlow",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
