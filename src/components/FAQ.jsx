4d7f999c1396bf77e75e57c99a065f9c93ad824c
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-24 20:58:56 +0530

    added faqlist

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FAQList } from "./FeaturesList";
 
 const FAQ = () => {
   return <div></div>;
--- src/components/FeaturesList.jsx
+++ src/components/FeaturesList.jsx
@@ -24,3 +24,23 @@ export const Features = [
     para: "A versatile framework combining Ethereum-based technology with powerful tools and applications.",
   },
 ];
+
+export const FAQList = [
+  {
+    question: "QUESTION 1",
+    answers:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum fringilla aliquam. Nullam in dui a ante laoreet consequat non a sem. Curabitur nec libero diam. Mauris consequat convallis urna.",
+  },
+
+  {
+    question: "QUESTION 2",
+    answers:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum fringilla aliquam. Nullam in dui a ante laoreet consequat non a sem. Curabitur nec libero diam. Mauris consequat convallis urna.",
+  },
+
+  {
+    question: "QUESTION 3",
+    answers:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum fringilla aliquam. Nullam in dui a ante laoreet consequat non a sem. Curabitur nec libero diam. Mauris consequat convallis urna.",
+  },
+];
