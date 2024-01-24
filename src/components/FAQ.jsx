99355e456e5e1fcf1dec27e26e85f2442c7938c5
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-24 21:34:54 +0530

    faq section done

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FAQList } from "./FeaturesList";
+import { FaArrowCircleDown } from "react-icons/fa";
 import { IoMdArrowDropdownCircle, IoMdArrowDropupCircle } from "react-icons/io";
 
 const FAQ = () => {
@@ -15,6 +16,19 @@ const FAQ = () => {
     <div className="py-10">
       <div className="container">
         <div className="flex flex-col justify-center items-center">
+          <div className="flex justify-center items-center gap-x-5 mb-5">
+            <h3 className="inline text-slate-200 text-xl px-8 py-3 bg-gradient-to-r from-pink-700 to-pink-500  rounded-full  font-primary-font font-bold leading-relaxed tracking-wider">
+              BENEFITS
+            </h3>
+
+            <a href="" className="text-slate-100">
+              <FaArrowCircleDown
+                size={18}
+                className="animate-bounce hover:text-blue-500 duration-200"
+              />
+            </a>
+          </div>
+
           {FAQList.map(({ question, answers }, index) => (
             <div
               key={index}
